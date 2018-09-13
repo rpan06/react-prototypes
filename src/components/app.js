70e83059bc852217486246455fddc44aa1e55ba9
Rachel Pan <rpan06@gmail.com>
2018-09-12 18:46:11 -0700

    functional-components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
+import React from 'react';
+import {Table} from "./table"
+
+const students = [
+    {
+        name: 'Peter Parker',
+        course: 'Intro to Photography',
+        grade: 85
+    },
+    {
+        name: 'Bruce Banner',
+        course: 'Psychology',
+        grade: 91
+    },
+    {
+        name: 'Tony Stark',
+        course: 'Mechanical Engineering',
+        grade: 100
+    }
+];
 
 
+export function App(){
+    return (
+        <div className="container">
+        <h1>Student Grade Table</h1>
+        <Table data={students}/>
+        </div>
+    )
+}
