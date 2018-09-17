01edddd220d702f99063b231ef5461a424bf6f8c
Rachel Pan <rpan06@gmail.com>
2018-09-17 14:44:25 -0700

    component-state

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,9 @@
+import React from 'react';
+import Stopwatch from './stopwatch'
 
-
+export const App = () => (
+    <div className='container'>
+        <h1 className='text-center'>StopWatch</h1>
+        <Stopwatch/>
+    </div>
+)
---
+++ src/components/format_time.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+
+export class FormatTime extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            hour: 0,
+            min: 0,
+            sec: 0,
+            ms: 0,
+        }
+        this.timerStyle = {
+            display: 'inline-block',
+            position: 'relative',
+            left: '50%',
+            transform: 'translate(-50%)',
+            width: '383px'
+        }
+    }
+    componentWillReceiveProps(nextProps){
+        const {elapsed} = nextProps;
+        const sec = elapsed/1000;
+        const min = sec/60;
+        this.setState({
+            hour: Math.floor(min / 60),
+            min: Math.floor(min % 60),
+            sec: Math.floor(sec % 60),
+            ms: Math.floor(elapsed % 100),
+        })
+    }
+    leadingZero(number){
+        return number < 10 ? `0${number}` : number;
+    }
+    trailingZero(number){
+        return number<10 ? `${number}0` : number;
+    }
+    render(){
+        const {hour, min, sec, ms} = this.state;
+        return (
+            <div style={this.timerStyle}>{this.leadingZero(hour)}:{this.leadingZero(min)}:{this.leadingZero(sec)}.{this.trailingZero(ms)}</div>
+        )
+    }
+}
+
+
+//make analog clock
+//click to switch between them
